ae92d34150adffdf03067006fa1ace7364a6afa4
Zaeb <zainab.hina05@gmail.com>
2026-07-13 21:14:40 +0500

    Time: 1 ms (27.09%) | Memory: 8.9 MB (58.76%) - LeetSync

--- 67-add-binary/add-binary.cpp
+++ 67-add-binary/add-binary.cpp
@@ -1,20 +1,21 @@
 class Solution {
-public:
-    string addBinary(string a, string b) {
-        string res;
-        int carry = 0;
-        int i = a.length() - 1;
-        int j = b.length() - 1;
+ public:
+  string addBinary(string a, string b) {
+    string ans;
+    int carry = 0;
+    int i = a.length() - 1;
+    int j = b.length() - 1;
 
-        while (i >= 0 || j >= 0 || carry) {
-            if (i >= 0) carry += a[i--] - '0';
-            if (j >= 0) carry += b[j--] - '0';
-
-            res += carry % 2 + '0';
-            carry /= 2;
-        }
-
-        reverse(begin(res), end(res));
-        return res;        
+    while (i >= 0 || j >= 0 || carry) {
+      if (i >= 0)
+        carry += a[i--] - '0';
+      if (j >= 0)
+        carry += b[j--] - '0';
+      ans += carry % 2 + '0';
+      carry /= 2;
     }
+
+    reverse(begin(ans), end(ans));
+    return ans;
+  }
 };
